835240f30e6707a71c2891d886df72d2778ea8ec
shiv kumar <shiv@vcanaglobal.com>
2024-09-11 15:36:55 +0530

    changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Template;
 use Illuminate\Http\Request;
 use Auth;
 class DashboardController extends Controller
@@ -17,7 +18,8 @@ class DashboardController extends Controller
 
     
     public function templateView(){
-        return view('dashboard.viewtemplate');
+        $templates = Template::get();
+        return view('dashboard.viewtemplate',compact('templates'));
     }
 
 }

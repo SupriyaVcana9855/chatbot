66e40fe2535f2e10f551aeb9b44423994226987c
supriyaVcana <supriya@vcanaglobal.com>
2024-09-13 11:30:54 +0530

    chatbot

---
+++ app/Models/BotUser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BotUser extends Model
+{
+    use HasFactory;
+    public function questionAnswer()
+    {
+         return $this->hasMany(QuestionAnswer::class,'bot_user_id');
+    }
+}
